c910d37d4ebcdd7fcfbe7ab73e43cea3ad253cba
jeffreydbower <jeff.d.bower@gmai.com>
2026-05-03 11:08:41 -0400

    Stacked basicLair test with legendary at bottom layer.

--- UndergroundSiteJoppaWorldBuilderExtension.cs
+++ UndergroundSiteJoppaWorldBuilderExtension.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using XRL;
 using XRL.Core;
 using XRL.World;
@@ -19,8 +20,7 @@ namespace SubterraneanSites
 
     public class RuntimeZoneBuilderInjectionSystem : IGameSystem
     {
-        //private const string TargetZoneId = "JoppaWorld.11.22.0.1.11";
-        //private const string TargetZoneName = "basic lair default clean test";
+        private const string TargetZoneId = "JoppaWorld.11.22.0.1.11";
 
         public override void Register(XRLGame game, IEventRegistrar registrar)
         {
@@ -30,6 +30,7 @@ namespace SubterraneanSites
         public override bool HandleEvent(BeforeZoneBuiltEvent zoneBuildEvent)
         {
             var Z = zoneBuildEvent.Zone;
+            var zId = Z.ZoneID;
 
             // Only test underground zones.
             if (Z.Z <= 10)
@@ -37,137 +38,99 @@ namespace SubterraneanSites
                 return true;
             }
 
-            int zoneSeed = StableHash(Z.ZoneID);
+            
+            int rawSeed = XRLCore.Core.Game.GetWorldSeed();
+            int zoneSeed = XRLCore.Core.Game.GetWorldSeed(TargetZoneId + rawSeed);
+            System.Random rng = new System.Random(zoneSeed);
+            int layers = rng.Next(1, 8); // 1 to 7 addtional layers
 
+            List<string> siteZoneIds = new List<string>();
+
+            string baseWorld = "JoppaWorld";
+            int parasangX = 11;
+            int parasangY = 22;
+            int zoneX = 0;
+            int zoneY = 1;
+            int startZ = 11;
+
+            siteZoneIds.Add(TargetZoneId);
+            for (int i = 1; i < layers; i++)
+            {
+                int z = startZ + i;
+                string zoneId =
+                    baseWorld + "." +
+                    parasangX + "." +
+                    parasangY + "." +
+                    zoneX + "." +
+                    zoneY + "." +
+                    z;
+                siteZoneIds.Add(zoneId);    
+            }
+
+            //foreach (string zoneId in siteZoneIds)
+            for (int layer_num = 0; layer_num < siteZoneIds.Count; layer_num++)
+            {
+                string zoneId = siteZoneIds[layer_num];
+
+                if (zId == zoneId)
+                {
+                    // this zone is one layer of the test site
+                    foreach (var cell in Z.GetCells())
+                    {
+                        cell.Clear();
+                    }
+
+                    var lair = new XRL.World.ZoneBuilders.BasicLair();
+                    lair.Table = "";        // default
+                    //lair.Table = "DynamicInheritsTable:Creature:Tier5";        // test creature table 
+                    lair.Adjectives = "";   // test adjetive
+                    lair.Stairs = "D";       // down stairs stairs
+                    lair.BuildZone(Z);
+
+                    if(zId == siteZoneIds[siteZoneIds.Count - 1])
+                    {
+                        var factionEncounters = new XRL.World.ZoneBuilders.FactionEncounters();
+                        factionEncounters.Chance = 100;
+                        factionEncounters.Rolls = 1;
+                        factionEncounters.Population = "GenericFactionPopulation";
+                        factionEncounters.BuildZone(Z);
+                    }
+
+                    The.ZoneManager.SetZoneName
+                    (
+                    Z.ZoneID,
+                    "Stacked Lair Test: Layer: " + (layer_num + 1) + " of " + siteZoneIds.Count,
+                    Proper: false
+                    );
+
+                    return true;
+
+                }
+            }
+            
+
+            //int zoneSeed = StableHash(Z.ZoneID);
+            //Tests that world seed is generated and constand succeeded
+            //below is the corect call
             //int rawSeed = XRLCore.Core.Game.GetWorldSeed();
             //int hash = XRLCore.Core.Game.GetWorldSeed(Z.ZoneID + rawSeed);
-
-            System.Random rng = new System.Random(zoneSeed);
-
-
             //Test for deterministic randon numbers
             //will repeat 2 new games 
             //if we use the zoneid as the rng seed
             //then the rolls and name should be the same in 
             // different games
-            int rollA = rng.Next(0, 2);      // 0 or 1
-            int rollB = rng.Next(1, 101);    // 1 to 100
-
-            int bucket = Math.Abs(zoneSeed % 100);
-
-
-            if (bucket < 50)
-            {
-                //return true;
-                foreach (var cell in Z.GetCells())
-                {
-                    cell.Clear();
-                }
-
-                The.ZoneManager.SetZoneName
-                (
-                     Z.ZoneID,
-                    "rng test zoneSeed " + zoneSeed + " A" + rollA + " B" + rollB,
-                    Proper: false
-                );
+            //System.Random rng = new System.Random(zoneSeed);
+           //int rollA = rng.Next(0, 2);      // 0 or 1
+            //int rollB = rng.Next(1, 101);    // 1 to 100
+            //int bucket = Math.Abs(zoneSeed % 100);
+            //if (bucket < 50)
+            //{
+            //    //inject
+            //}
 
 
-                var lair = new XRL.World.ZoneBuilders.BasicLair();
-                 //lair.Table = "";        // default
-                lair.Table = "DynamicInheritsTable:Creature:Tier5";        // test creature table 
-                lair.Adjectives = "";   // test adjetive
-                lair.Stairs = "D";       // down stairs stairs
-                lair.BuildZone(Z);
-
-                var factionEncounters = new XRL.World.ZoneBuilders.FactionEncounters();
-                factionEncounters.Chance = 100;
-                factionEncounters.Rolls = 1;
-                factionEncounters.Population = "GenericFactionPopulation";
-                factionEncounters.BuildZone(Z);
-                return true;
-            }
-
-  
-
-            /*
-            if (zoneBuildEvent.Zone.ZoneID != TargetZoneId)
-            {
-                return true;
-            }
-
-            // Set test name
-            The.ZoneManager.SetZoneName(
-                TargetZoneId,
-                TargetZoneName,
-                Proper: false
-            );
-            */
-
-            // --- PATTERN: String-based builder invocation ---
-            // Use this for builders that:
-            // - do not require parameters
-            // - are designed to be invoked by name
-            // Example: SnapjawStockadeMaker
-            /*
-            ZoneManager.ApplyBuilderToZone(
-                "SnapjawStockadeMaker",
-                zoneBuildEvent.Zone
-            );
-            */
-
-            // --- TEST 1 (overlay BananaGrove) ---
-            /*
-            var builder = new XRL.World.ZoneBuilders.BananaGrove();
-            builder.Underground = true;
-            builder.BuildZone(zoneBuildEvent.Zone);
-            
-            // --- TEST 2: clear zone first, then apply BananaGrove ---
-            var Z = zoneBuildEvent.Zone;
-            // Clear all cells (terrain + objects)
-            foreach (var cell in Z.GetCells())
-            {
-                cell.Clear();
-            }
-            var cleanBuilder = new XRL.World.ZoneBuilders.BananaGrove();
-            cleanBuilder.Underground = true;
-            cleanBuilder.BuildZone(Z);
-            */
-/*
-            // --- Basic Lair Test Defaults Clear Screen ---
-            var Z = zoneBuildEvent.Zone;
-            // Clear all cells (terrain + objects)
-            foreach (var cell in Z.GetCells())
-            {
-                cell.Clear();
-            }
-            var lair = new XRL.World.ZoneBuilders.BasicLair();
-            //lair.Table = "";        // default
-            lair.Table = "DynamicInheritsTable:Creature:Tier5";        // test creature table 
-            lair.Adjectives = "workshop";   // test adjetive
-            lair.Stairs = "D";       // down stairs stairs
-            lair.BuildZone(Z);
-
-            var factionEncounters = new XRL.World.ZoneBuilders.FactionEncounters();
-            factionEncounters.Chance = 100;
-            factionEncounters.Rolls = 1;
-            factionEncounters.Population = "GenericFactionPopulation";
-            factionEncounters.BuildZone(Z);
-*/
             return true;
         }
-
-        private static int StableHash(string text)
-        {
-            unchecked
-            {
-                int hash = 23;
-                foreach (char c in text)
-                {
-                    hash = hash * 31 + c;
-                }
-                return hash & 0x7fffffff;
-            }
-        }
-
     }
+    
 }
